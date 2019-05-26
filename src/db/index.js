4d1d56cc8289892f4144e1b2491ae5a12e1d9520
Caroline Pisano <pisanocaroline@gmail.com>
2019-05-26 16:05:07 -0400

    Added in firebase credentials and parsed csv data :package:

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,24 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("../../firebase-credentials.json");
-
+const fs = require('fs');
+// const csv = require('csv');
+const csvParser = require('csv-parser');
 // initialize firebase store
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
+// we initialize an empty array to contain our data
+const results = [];
 
+fs.createReadStream(input)
+  .pipe(csvParser({
+    separator: ',',
+  }))
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    console.log(results);
+  });
 // import the db from any file to access firebase!
 module.exports = db;
