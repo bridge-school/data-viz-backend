a6132148922fe5fd441e557861be0e266463dd02
leandra <leandra.reid@gmail.com>
2019-05-30 07:00:42 -0400

    get request working for all cohorts

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,5 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("../../firebase-credentials.json");
-// const fs = require('fs');
-
-// const csvParser = require('csv-parser');
-// initialize firebase store
 
 
 admin.initializeApp({
@@ -11,16 +7,5 @@ admin.initializeApp({
 });
 
 const db = admin.firestore();
-// we initialize an empty array to contain our data
-// const results = [];
 
-// fs.createReadStream(input)
-//   .pipe(csvParser({
-//     separator: ',',
-//   }))
-//   .on('data', (data) => results.push(data))
-//   .on('end', () => {
-//     console.log(results);
-//   });
-// import the db from any file to access firebase!
 module.exports = db;
--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ const { errorHandler } = require("./middleware/error-handler");
 const app = express();
 
 // Imports for parsing data
-const fs = require('fs');
-const csvParser = require('csv-parser');
+// const fs = require('fs');
+// const csvParser = require('csv-parser');
 // const input = "./src/data.csv";
-const db = require('./db/index.js');
+// const db = require('./db/index.js');
 
 // The port the express app will listen on
 const port = process.env.PORT || 8081;
@@ -36,8 +36,14 @@ if (process.env.NODE_ENV !== "test") {
 
 
 
+let string1="";
 
-
+const scrub = (object1) =>{
+  Object.keys(object1).forEach(function (key) {
+    console.log(key, object1[key]);
+    // `This is ${key}: ${object1[key]}`;
+  });
+};
 
 
 // we initialize an empty array to contain our data
@@ -46,21 +52,29 @@ if (process.env.NODE_ENV !== "test") {
 // fs.createReadStream(input)
 //   .pipe(csvParser({
     // we separate our csv data based on comma separation
-    //separator: ','
+  //   separator: ','
   // }))
-  //convert data format here --> create new variable
-  //interate through results array in end stream
+  // convert data format here --> create new variable
+  // interate through results array in end stream
   // .on('data', (data) => results.push(data))
   // .on('end', () => {
-    //loop through rows, set document within
-    //foreach
-    //each document has unique identifier
+    // loop through rows, set document within
+    // foreach
+    // each document has unique identifier
+    // console.log(results.forEach(applicant => {console.log(scrub(applicant))}));
+
+
+    // console.log(results.map(applicant => scrub(applicant)));
+
     // results.forEach(applicant => {
-      // console.log(applicant)
-      // db.collection('applicants').doc(applicant["Applicant ID"]).set({
-      //   ...applicant
-      // })
+    //   console.log(applicant)
+    //   db.collection('candidates').doc(applicant["Applicant ID"]).set({
+    //     ...applicant
+    //   })
     // })
+
+    //results is an array: this is how to navigate through it
+    // console.log(results[0]['How do you identify?'])
   // });
 
 
@@ -68,24 +82,29 @@ if (process.env.NODE_ENV !== "test") {
 
 
 
+
+
 //retrieving the 'applicants' collection from the db
-let applicantsRef = db.collection('applicants');
+// let applicantsRef = db.collection('applicants');
 
 //accessing the applicants data in the db with the firebase get promise, then can access data within!
 //need to call .data() method on the firebase information, then can pass in a key inside of square brackets to access each property
 //Firebase has sort/search functionality built in, that might be a route (hehe) to explore: search by cohort, then pass that to the front end
 
-let allApplicants = applicantsRef.get()
-  .then(snapshot => {
-    snapshot.forEach(doc => {
-      // console.log(doc.id, '=>', doc.data());
-      // console.log(doc.data()["Cohort"]);
-      console.log(doc.data()["Do you identify as any of the following? Please check all that apply."]);
-    });
-  })
-  .catch(err => {
-    console.log('Error getting documents', err);
-  });
+// let allApplicants = applicantsRef.get()
+//   .then(snapshot => {
+//     snapshot.forEach(doc => {
+//       console.log(doc.data());
+//       // console.log(doc.id, '=>', doc.data());
+//       // console.log(doc.data()["Cohort"]);
+//       console.log(doc.data()["Do you identify as any of the following? Please check all that apply."]);
+//     });
+//   })
+//   .catch(err => {
+//     console.log('Error getting documents', err);
+//   });
+
+
 
 
 module.exports = {
