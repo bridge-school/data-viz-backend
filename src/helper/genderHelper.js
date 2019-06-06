55742075fe6f527f72878a4c984613aade899f65
leandra <leandra.reid@gmail.com>
2019-06-06 06:53:49 -0400

    adding work on genderhelper so far

--- src/helper/genderHelper.js
+++ src/helper/genderHelper.js
@@ -1,22 +1,17 @@
-const express = require("express");
 const data = require('../mockData');
-const port = process.env.PORT || 8081;
-const app = express();
 
-if (process.env.NODE_ENV !== "test") {
-    app.listen(port, () => {
-        (`🎧 Listening at http://localhost:${port}/`);
-    });
-}
 
 const genderIdentity = (cohort) =>{
     return cohort
     //TODO: make the filter dynamic, or make cohort filter before
     .filter(student => student.cohort === 'cohort-7')
-    .reduce((obj, newStudent) => ({
-        ...obj,
-        [newStudent.gender] : (obj[newStudent.gender] || 0) + 1
-    }), {})
+    .map(student => student.gender.split(","))
+    // .reduce((obj, newStudent) =>
+    // ({
+    //     ...obj,
+    //     // let split = newStudent.gender.split()
+    //     [newStudent.gender] : (obj[newStudent.gender] || 0) + 1
+    // }), {})
 };
 
 console.log(genderIdentity(data.data));
