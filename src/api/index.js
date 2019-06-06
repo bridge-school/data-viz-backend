fe865b037e559db435f060690f13e826d8a53c6d
Rachel <835671347@qq.com>
2019-06-06 06:52:12 -0400

    [Feature][#2,3,4,5] Create routes for all cohorts & single cohort and connect with helper functions (#12)
    * Create routes
    
    * Set up helper functions

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 
-const { healthRouter } = require('../routes/health/health.router');
-const cohortRouter = require('../routes/cohorts');
-
+const { healthRouter } = require("../routes/health/health.router");
+const { cohortsRouter } = require("../routes/cohorts/cohorts.router");
 
 const router = express.Router();
 router.use("/health", healthRouter);
-router.use('/cohorts', cohortRouter);
+router.use("/cohorts", cohortsRouter);
 
 module.exports = router;
