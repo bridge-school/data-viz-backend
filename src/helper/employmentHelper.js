f084e084ee5eaca9c36ae200a8ac52d615ae26f9
Caroline Pisano <pisanocaroline@gmail.com>
2019-06-07 09:45:50 -0400

    Output is now as expected but am dealing with an unhandled error - breaking change atm

--- src/helper/employmentHelper.js
+++ src/helper/employmentHelper.js
@@ -1,10 +1,22 @@
+const express = require("express");
+const data = require("./mockData.js");
+const port = process.env.PORT || 8081;
+const app = express();
+
+if (process.env.NODE_ENV !== "test") {
+  app.listen(port, () => {
+    `🎧 Listening at http://localhost:${port}/`;
+  });
+}
 const employmentHelper = (data) => {
-    return data.reduce((obj, employment) => {
+    const employmentTally =  data.reduce((obj, employment) => {
         return {
             ...obj,
             [employment.employment_status] : (obj[employment.employment_status] || 0) +1
         }
-    }, {})
+    }, [])
+    return Object.entries(employmentTally).map(([key, value]) => ({label:key,value:value}));
+
 }
 console.log(employmentHelper(data.data));
 
