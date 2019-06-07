39b35a6b7f5b0db614e34e64b020888ad2e77aa0
Caroline Pisano <pisanocaroline@gmail.com>
2019-06-07 12:26:58 -0400

    :necktie: Employment parser produces desired result, node issue fixed

--- src/helper/employmentHelper.js
+++ src/helper/employmentHelper.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const data = require("./mockData.js");
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 2727;
 const app = express();
 
 if (process.env.NODE_ENV !== "test") {
@@ -9,14 +9,16 @@ if (process.env.NODE_ENV !== "test") {
   });
 }
 const employmentHelper = (data) => {
+    // first we use .reduce to tally each response for employment_status
     const employmentTally =  data.reduce((obj, employment) => {
         return {
             ...obj,
             [employment.employment_status] : (obj[employment.employment_status] || 0) +1
         }
     }, [])
-    return Object.entries(employmentTally).map(([key, value]) => ({label:key,value:value}));
-
+    // result of above reduce produces and object where the key is the employment status and value is tally
+    // we format this into {label: 'type of employment status', value:'tally'} using object.entries and .map
+    return Object.entries(employmentTally).map(([label, value]) => ({label,value}));
 }
 console.log(employmentHelper(data.data));
 
