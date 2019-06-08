11ddb069b2fd6a57fd2132f2f41daf98b2277d66
qiuqili <835671347@qq.com>
2019-06-07 21:46:11 -0400

    Add cohortsHelper function

--- src/helper/cohortsHelper.js
+++ src/helper/cohortsHelper.js
@@ -1,3 +1,14 @@
 module.exports = function cohortsHelper(cohorts) {
-  return cohorts;
+  //count number of instance
+  const counted = cohorts.reduce(function(obj, id) {
+    id in obj ? obj[id]++ : (obj[id] = 1);
+    return obj;
+  }, {});
+  //format in to new object with key "lable" and "value"
+  const result = Object.entries(counted).map(([lable, value]) => ({
+    lable,
+    value
+  }));
+
+  return result;
 };
