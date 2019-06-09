bfceaf6d892b431eb745a9e58930132f6f30df9b
Caroline Pisano <37511886+caropi@users.noreply.github.com>
2019-06-09 12:35:28 -0400

    [Feature/Employment Parser][#2.5] Created parser for back-end to connect to front-end  (#16)
    * Added mockData to helper and added to .gitignore
    
    * Added reducer - currently broken and counting everything except first object
    
    * Fixed skipped 0 index
    
    * Output is now as expected but am dealing with an unhandled error - breaking change atm
    
    * :necktie: Employment parser produces desired result, node issue fixed
    
    * Removed express listener

--- src/helper/employmentHelper.js
+++ src/helper/employmentHelper.js
@@ -1,3 +1,16 @@
 module.exports = function employmentHelper(data) {
-  return data;
+  // first we use .reduce to tally each response for employment_status
+  const employmentTally = data.reduce((obj, employment) => {
+    return {
+      ...obj,
+      [employment.employment_status]:
+        (obj[employment.employment_status] || 0) + 1
+    };
+  }, []);
+  // result of above reduce produces and object where the key is the employment status and value is tally
+  // we format this into {label: 'type of employment status', value:'tally'} using object.entries and .map
+  return Object.entries(employmentTally).map(([label, value]) => ({
+    label,
+    value
+  }));
 };
